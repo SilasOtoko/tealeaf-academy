b0167810b5529920dbd50a6e881a0b7b712254f8
Josiah Schaefer <penname.silas@gmail.com>
2015-06-08 20:54:34 -0400

    adding most functionality to blackjack game

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -5,19 +5,108 @@ require 'pry'
 
 set :sessions, true
 
-get '/inline' do 
-  "Hi, directly from the action!"
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == 'A'
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    arr.select{|element| element == 'A'}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+
+  def card_image(card)
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'
+      end
+    end
+
+
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
 end
 
-get '/template' do
-  erb :mytemplate
+
+before do
+  @show_hit_or_stay_buttons = true
 end
 
-get '/nested_template' do
-  erb :"/users/profile"
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
 end
 
-get '/nothere' do
-  redirect '/inline'
+get '/new_player' do
+  erb :new_player
 end
 
+post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  # deck
+  suits = ['H', 'D', 'S', 'C']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+  
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    @success = "Congratulations! #{session[:player_name]} hit Blackjack!"
+  elsif calculate_total(session[:player_cards]) > 21
+    @error = "Sorry, it looks like #{session[:player_name]} busted."
+    @show_hit_or_stay_buttons = false
+  end
+  erb :game
+end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay"
+  @show_hit_or_stay_buttons = false
+  erb :game
+end
